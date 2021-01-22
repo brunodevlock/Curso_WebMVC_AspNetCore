b31d390d86629003dfd085f27a31723db9b4ab83
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-01-22 20:06:23 -0300

    SellerFormViewModel + Lista

--- SalesWebMVC/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -12,10 +13,12 @@ namespace SalesWebMvc.Controllers
     {
 
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
 
@@ -29,7 +32,13 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel
+            {
+                Departments = departments
+            };
+
+            return View(viewModel);
         }
 
         [HttpPost]
